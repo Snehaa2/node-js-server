8ac7469cb42d891f7aa99f7c7a9a50bcbf9598ed
Snehaa2 <snehac6194@gmail.com>
2025-04-22 15:21:51 +0530

    first commit

---
+++ nodejs-static-server-main/server.js
@@ -0,0 +1,38 @@
+const http = require("http"); 
+const fs = require("fs"); 
+const path = require("path"); 
+const server = http.createServer((req, res) => { 
+let filePath = req.url === "/" ? "./public/index.html" : 
+`./public${req.url}`;  
+let extname = path.extname(filePath); 
+let contentType = "text/html"; 
+// Set content type based on file extension
+switch (extname) { 
+    case ".css":  
+           contentType = "text/css";  
+           break; 
+    case ".js":  
+            contentType = "text/javascript";  
+            break; 
+    case ".png":  
+    case ".jpg": 
+    case ".jpeg": 
+           contentType = `image/${extname.slice(1)}`;  
+           break;  
+} 
+
+fs.readFile(filePath, (err, data) => { 
+   if (err) { 
+       res.writeHead(404, { "Content-Type": "text/html" });  
+       res.end("<h1>404 Not Found</h1>");  
+} else {  
+       res.writeHead(200, { "Content-Type": contentType }); 
+       res.end(data); 
+} 
+   }); 
+});
+// 🔥 Add this at the end of the file
+const PORT = 3000;
+server.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
